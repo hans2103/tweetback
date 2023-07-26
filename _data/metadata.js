504b6bd1a6cf802bb0b3a5e94b7acf6d72fa6f95
Hans Kuijpers <hans2103@gmail.com>
2023-07-26 13:09:02 +0200

    import twitter data

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,7 +1,7 @@
 let data = {
-	username: "eleven_ty", // No leading @ here
-	homeLabel: "11ty.dev",
-	homeUrl: "https://www.11ty.dev/",
+	username: "hans2103", // No leading @ here
+	homeLabel: "hans2103.nl",
+	homeUrl: "https://tweets-by.hans2103.nl/",
 };
 
 data.avatar = `https://v1.indieweb-avatar.11ty.dev/${encodeURIComponent(data.homeUrl)}/`;
